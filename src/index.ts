793549f86d456c352e27c7ff690cc42148b0ccd6
Niels Becker <aaqqws@googlemail.com>
2020-03-20 13:26:05 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { AgentManager } from "./gen/AgentManager";
+export { ConnectionActive } from "./gen/Connection-Active";
+export { DeviceBridge } from "./gen/Device-Bridge";
+export { DeviceGeneric } from "./gen/Device-Generic";
+export { DeviceStatistics } from "./gen/Device-Statistics";
+export { DeviceVeth } from "./gen/Device-Veth";
+export { DeviceWired } from "./gen/Device-Wired";
+export { DeviceWireless } from "./gen/Device-Wireless";
+export { Device } from "./gen/Device";
+export { DHCP4Config } from "./gen/DHCP4Config";
+export { DHCP6Config } from "./gen/DHCP6Config";
+export { DnsManager } from "./gen/DnsManager";
+export { IP4Config } from "./gen/IP4Config";
+export { IP6Config } from "./gen/IP6Config";
+export { NetworkManager } from "./gen/NetworkManager";
+export { SettingsConnection } from "./gen/Settings-Connection";
+export { Settings } from "./gen/Settings";
+
+export * as Enums from "./enums";
